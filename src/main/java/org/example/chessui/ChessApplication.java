046c07c25b1dc2155849b8a92d3111410f2cbb96
houssein badra <badrahsein@gmail.com>
2025-05-27 19:49:28 +0300

    add alpha beta pruning

--- src/main/java/org/example/chessui/ChessApplication.java
+++ src/main/java/org/example/chessui/ChessApplication.java
@@ -7,7 +7,7 @@ import javafx.stage.Stage;
 
 public class ChessApplication extends Application {
     @Override public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("player_vs_ai.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
         Scene scene = new Scene(loader.load());
         scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
         stage.setTitle("ChessFX");
